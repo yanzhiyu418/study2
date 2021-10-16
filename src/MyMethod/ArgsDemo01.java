0ee616b6bc0121679910f33750efc7445107c200
yanzhiyu418 <71864954+yanzhiyu418@users.noreply.github.com>
2021-10-16 21:50:41 +0800

    study

---
+++ src/MyMethod/ArgsDemo01.java
@@ -0,0 +1,19 @@
+package MyMethod;
+/*
+对于基本数据类型的参数，形式参数的改变，不影响世纪参数的值
+*/
+public class ArgsDemo01 {
+    public static void main(String[] args) {
+        int number=100;
+        System.out.println("调用change方法前:"+number);
+        change(number);
+        System.out.println("调用change方法后:"+number);
+    }
+
+    public static void change (int number){
+        number=200;
+
+    }
+
+}
+
